dcec69cf6e679c9ef1c2b32e931d217163182e10
Jaroslaw Krzywanski <jkrzywanski@tlen.pl>
2023-04-15 21:36:28 +0200

    Zadanie - dzień 3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+bool kobieta = true;
+int age = 33;
+
+string message1 = "Kobieta poniżej 30 lat";
+string message2 = "Ewa, lat 33";
+string message3 = "Niepełnoletni męzczyzna";
+
+
+
+if ((age < 30) & (kobieta))
+{
+    Console.WriteLine(message1);
+}
+else
+{
+    if ((name == "Ewa") && (age == 33))
+    {
+        Console.WriteLine(message2);
+    }
+    else
+    {
+        Console.WriteLine(message3);
+    }
+}
