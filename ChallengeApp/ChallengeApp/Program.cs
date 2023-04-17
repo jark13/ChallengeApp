bd4926497e364206fd7941cb45d2c3cda86594db
Jaroslaw Krzywanski <jkrzywanski@tlen.pl>
2023-04-17 15:43:08 +0200

    Zadnie domowe - dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,25 +1,85 @@
-﻿//Zadanie - dzień 4
-string name = "Ewa";
-bool kobieta = true;
-int age = 33;
+﻿int number = 4566;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToArray();
 
-string message1 = "Kobieta poniżej 30 lat";
-string message2 = "Ewa, lat 33";
-string message3 = "Niepełnoletni męzczyzna";
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
 
-
-if ((age < 30) & (kobieta))
+foreach (char letter in letters)
 {
-    Console.WriteLine(message1);
-}
-else
-{
-    if ((name == "Ewa") && (age == 33))
+    if (letter == '0')
     {
-        Console.WriteLine(message2);
+        counter0++;
     }
-    else
+    else if (letter == '1')
     {
-        Console.WriteLine(message3);
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
     }
 }
+
+Console.Write("Wyniki dla liczby: ");
+Console.WriteLine(numberAsString);
+
+Console.Write("0 => ");
+Console.WriteLine(counter0);
+Console.Write("1 => ");
+Console.WriteLine(counter1);
+Console.Write("2 => ");
+Console.WriteLine(counter2);
+Console.Write("3 => ");
+Console.WriteLine(counter3);
+Console.Write("4 => ");
+Console.WriteLine(counter4);
+Console.Write("5 => ");
+Console.WriteLine(counter5);
+Console.Write("6 => ");
+Console.WriteLine(counter6);
+Console.Write("7 => ");
+Console.WriteLine(counter7);
+Console.Write("8 => ");
+Console.WriteLine(counter8);
+Console.Write("9 => ");
+Console.WriteLine(counter9);
+
+
+//Console.WriteLine("Wyniki dla liczby numberAsString", numberAsString);
